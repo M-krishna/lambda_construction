343d638cc8d1a55ea9082e8b286bb5b010ac7b92
Krishna <krishnamurugan589@gmail.com>
2025-04-12 19:20:09 +0530

    chore: added one test for parser

--- test/test_parser.py
+++ test/test_parser.py
@@ -66,14 +66,36 @@ class TestParser(unittest.TestCase):
         parser = Parser(tokens)
         parser.parse()
         ast: List[Expression] = parser.get_ast()
-        self.assertIsInstance(ast[0], LambdaApplicationNode)
 
+        self.assertIsInstance(ast[0], LambdaApplicationNode)
         self.assertIsInstance(ast[0].left, LambdaAbstractionNode)
         self.assertIsInstance(ast[0].right, VariableNode)
         self.assertEqual(ast[0].left.param, "x")
         self.assertIsInstance(ast[0].left.body, VariableNode)
         self.assertEqual(ast[0].left.body.value, "x")
         self.assertEqual(ast[0].right.value, "y")
+
+    def test_application_3(self):
+        source = "(fn x.x) (fn y.y)"
+
+        lexer = Lexer(source)
+        lexer.tokenize()
+        tokens: List[Token] = lexer.get_tokens()
+
+        parser = Parser(tokens)
+        parser.parse()
+        ast: List[Expression] = parser.get_ast()
+
+        self.assertIsInstance(ast[0], LambdaApplicationNode)
+        self.assertIsInstance(ast[0].left, LambdaAbstractionNode)
+        self.assertIsInstance(ast[0].right, LambdaAbstractionNode)
+        self.assertEqual(ast[0].left.param, "x")
+        self.assertEqual(ast[0].right.param, "y")
+        self.assertIsInstance(ast[0].left.body, VariableNode)
+        self.assertIsInstance(ast[0].right.body, VariableNode)
+        self.assertEqual(ast[0].left.body.value, "x")
+        self.assertEqual(ast[0].right.body.value, "y")
+
 
 if __name__ == "__main__":
     unittest.main()
